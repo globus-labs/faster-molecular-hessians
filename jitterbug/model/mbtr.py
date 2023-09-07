ce124ded21b43cc6ea26018fb42698c0542d140f
Logan Ward <ward.logan.t@gmail.com>
2023-09-07 12:05:58 -0400

    Add ability to compute Hessian from MBTR

--- jitterbug/model/mbtr.py
+++ jitterbug/model/mbtr.py
@@ -6,12 +6,17 @@ problem as a simple "molecule->energy" learning problem. Other methods,
 such as SOAP, provided atomic-level features that must require an
 extra step "molecule->atoms->energy/atom->energy".
 """
+import shutil
+from shutil import rmtree
 
 from ase.calculators.calculator import Calculator, all_changes
+from ase.vibrations import Vibrations
 from ase import Atoms
 from sklearn.linear_model import LinearRegression
 from dscribe.descriptors import MBTR
 import numpy as np
+
+from jitterbug.model.base import EnergyModel
 
 
 class MBTRCalculator(Calculator):
@@ -39,6 +44,8 @@ class MBTRCalculator(Calculator):
 
         # If desired, compute forces numerically
         if 'forces' in properties:
+            # calculate_numerical_forces use that the calculation of the input atoms,
+            #  even though it is a method of a calculator and not of the input atoms :shrug:
             temp_atoms: Atoms = atoms.copy()
             temp_atoms.calc = self
             self.results['forces'] = self.calculate_numerical_forces(temp_atoms)
@@ -58,3 +65,30 @@ class MBTRCalculator(Calculator):
         # Compute the descriptors and use them to fit the model
         desc = self.parameters['descriptor'].create(train_set)
         self.parameters['model'].fit(desc, energies)
+
+
+class MBTREnergyModel(EnergyModel):
+    """Use the MBTR representation to model the potential energy surface
+
+    Args:
+        calc: Calculator used to fit the potential energy surface
+        reference: Reference structure at which we compute the Hessian
+    """
+
+    def __init__(self, calc: MBTRCalculator, reference: Atoms):
+        super().__init__()
+        self.calc = calc
+        self.reference = reference
+
+    def train(self, data: list[Atoms]) -> MBTRCalculator:
+        self.calc.train(data)
+        return self.calc
+
+    def mean_hessian(self, model: MBTRCalculator) -> np.ndarray:
+        self.reference.calc = model
+        try:
+            vib = Vibrations(self.reference, name='mbtr-temp')
+            vib.run()
+            return vib.get_vibrations().get_hessian_2d()
+        finally:
+            rmtree('mbtr-temp', ignore_errors=True)
--- tests/models/test_mbtr.py
+++ tests/models/test_mbtr.py
@@ -1,7 +1,7 @@
 """Test for a MBTR-based energy model"""
 import numpy as np
 
-from jitterbug.model.mbtr import MBTRCalculator
+from jitterbug.model.mbtr import MBTRCalculator, MBTREnergyModel
 
 
 def test_model(train_set):
@@ -18,3 +18,14 @@ def test_model(train_set):
     # See if force calculation works
     forces = test_atoms.get_forces()
     assert forces.shape == (3, 3)  # At least make sure we get the right shape, values are iffy
+
+
+def test_hessian(train_set):
+    """See if we can compute the Hessian"""
+    calc = MBTRCalculator()
+    model = MBTREnergyModel(calc, train_set[0])
+
+    # Run the fitting
+    hess_model = model.train(train_set)
+    hess = model.mean_hessian(hess_model)
+    assert hess.shape == (9, 9)
