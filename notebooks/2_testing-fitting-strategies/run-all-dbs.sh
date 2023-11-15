62fa41b1168a219ce27ed76e707f56febf93aeeb
Logan Ward <ward.logan.t@gmail.com>
2023-11-15 14:20:35 -0500

    First notebook for comparing sampling methods

--- notebooks/2_testing-fitting-strategies/run-all-dbs.sh
+++ notebooks/2_testing-fitting-strategies/run-all-dbs.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 notebook=$1
-dbs=$(find ../1_explore-sampling-methods/data/ -name "caffine_pm7_None*.db")
+dbs=$(find ../1_explore-sampling-methods/data/ -name "caffeine_pm7_None*.db")
 for db in $dbs; do
   echo $db
-  papermill -p db_path $db $notebook -
+  papermill -p db_path "$db" -p max_size 5000 $notebook last.html
 done
