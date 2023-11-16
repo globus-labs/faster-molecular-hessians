5862dd9760cadb1defb9a5de569862feaf73c18d
Logan Ward <ward.logan.t@gmail.com>
2023-11-16 09:39:19 -0500

    Add enthalpy to the comaprison

--- notebooks/2_testing-fitting-strategies/run-all-dbs.sh
+++ notebooks/2_testing-fitting-strategies/run-all-dbs.sh
@@ -4,5 +4,5 @@ notebook=$1
 dbs=$(find ../1_explore-sampling-methods/data/ -name "caffeine_pm7_None*.db")
 for db in $dbs; do
   echo $db
-  papermill -p db_path "$db" -p max_size 5000 $notebook last.html
+  papermill -p db_path "$db" -p max_size 5000 $notebook last.ipynb
 done
