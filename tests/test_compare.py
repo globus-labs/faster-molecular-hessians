8f84e55ee87c095279be9fbf74d737a6609ac7d6
Logan Ward <ward.logan.t@gmail.com>
2023-12-05 07:25:02 -0500

    Add scaling for vibrational frequencies

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -19,3 +19,18 @@ def test_compare(example_hess):
     assert comp.zpe_error == 0.
     assert np.ndim(comp.cp_error) == 1
     assert np.mean(comp.cp_error) == 0.
+
+
+def test_scaling(example_hess):
+    # Make sure scaling the Hessian has the target effect
+    comp = compare_hessians(example_hess.get_atoms(), example_hess.get_hessian_2d(), example_hess.get_hessian_2d() * 1.1)
+    assert comp.zpe_error > 0
+
+    # ... and that it can be repaired
+    comp = compare_hessians(example_hess.get_atoms(), example_hess.get_hessian_2d(), example_hess.get_hessian_2d() * 1.1, scale_factor=1. / np.sqrt(1.1))
+    assert np.isclose(comp.zpe_error, 0)
+
+    # ... and that it can be repaired, automatically
+    comp = compare_hessians(example_hess.get_atoms(), example_hess.get_hessian_2d(), example_hess.get_hessian_2d() * 1.1, scale_factor=None)
+    assert np.isclose(comp.zpe_error, 0)
+    assert np.isclose(comp.scale_factor, 1. / np.sqrt(1.1))
