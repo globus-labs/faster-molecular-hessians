18a0c6df633b8d3a9aac769afc878ff3e05d458f
Logan Ward <WardLT@users.noreply.github.com>
2023-12-06 13:06:37 -0500

    Separate relaxation settings from hessian settings (#29)
    * Separate relaxation method from Hessian method
    
    * Move the data directory
    
    * Update sampling notebooks, use uniform dist not exp
    
    Exponential distribution was generating too large
    of displacements, leading to the runs not converging
    
    * Update rotation test
    
    * Update MBTR notebook
    
    * Update with new file naming convention

--- notebooks/1_explore-sampling-methods/run-all-methods.sh
+++ notebooks/1_explore-sampling-methods/run-all-methods.sh
@@ -1,23 +1,25 @@
 #! /bin/bash
 
 xyz=../data/exact/caffeine_pm7_None.xyz
+method='pm7/None'
+
 for step_size in 0.02; do
     # Do the randomized methods
-    for method in 0_random-directions-same-distance.ipynb 1_random-directions-variable-distance.ipynb; do
-        papermill -p starting_geometry $xyz -p step_size $step_size $method last.ipynb
+    for notebook in 0_random-directions-same-distance.ipynb 1_random-directions-variable-distance.ipynb; do
+        papermill -p starting_geometry $xyz -p method $method -p step_size $step_size $notebook last.ipynb
     done
     
     # Test with different reductions for "along axes"
     notebook=2_displace-along-axes.ipynb
     for n in 2 4 8; do
-        papermill -p starting_geometry $xyz -p perturbs_per_evaluation $n -p step_size $step_size $notebook last.ipynb
+        papermill -p starting_geometry $xyz -p method $method -p perturbs_per_evaluation $n -p step_size $step_size $notebook last.ipynb
     done
 done
 
 # Test with the vibrational modes
 notebook=3_displace-along-vibrational-modes.ipynb
-for step_size in 0.001 0.002; do
+for step_size in 0.001 0.002; do  # These step sizes are energy scales in eV, not distances in Angstrom as above
     for n in 16 32 64; do
-        papermill -p starting_geometry $xyz -p perturbs_per_evaluation $n -p step_size $step_size $notebook last.ipynb
+        papermill -p starting_geometry $xyz -p method $method -p perturbs_per_evaluation $n -p step_size $step_size $notebook last.ipynb
     done
 done
