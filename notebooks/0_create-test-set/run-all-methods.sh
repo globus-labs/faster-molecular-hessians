3f0d8c219e8410e7aa03fddf7a7833b327b9946b
Logan Ward <ward.logan.t@gmail.com>
2023-12-06 07:38:51 -0500

    Separate relaxation method from Hessian method

--- notebooks/0_create-test-set/run-all-methods.sh
+++ notebooks/0_create-test-set/run-all-methods.sh
@@ -1,18 +1,17 @@
 #! /bin/bash
 
-molecule=butanol
-methods="pm7//None xtb//None hf//cc-pvtz b3lyp//cc-pvtz wb97x-d//cc-pvtz m062x//cc-pvtz ccsd(t)//cc-pvdz"
+molecule=water
+relax_method="b3lyp/cc-pvtz"
+
+hess_methods="pm7/None xtb/None hf/cc-pvtz b3lyp/cc-pvtz wb97x-d/cc-pvtz m062x/cc-pvtz ccsd(t)/cc-pvdz"
 deltas="0.04 0.02 0.01 0.005 0.0025"
 
-#methods="ccsd(t)//cc-pvtz"
+#hess_methods="ccsd(t)/cc-pvtz"
 #deltas=0.005
 
 notebook=0_get-exact-answer.ipynb
-for name in $methods; do
-    echo $name
+for method in $hess_methods; do
     for delta in $deltas; do
-        method=$(echo $name | cut -d "/" -f 1)
-        basis=$(echo $name | cut -d "/" -f 3)
-        papermill -p method $method -p basis $basis -p delta $delta -p molecule_name $molecule $notebook live.ipynb
+        papermill -p hess_method $method -p relax_method $relax_method -p delta $delta -p molecule_name $molecule $notebook live.ipynb
     done
 done
