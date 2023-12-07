16ca719438c13319cda20d3cc9616e464eef6956
Logan Ward <ward.logan.t@gmail.com>
2023-12-07 10:28:54 -0500

    Test the parsl config loader

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,3 +12,13 @@ def test_exact_solver(xyz_path):
                 str(xyz_path), '--exact', '--method', 'pm7', 'None'
             ])
     assert (Path('run') / 'water' / 'pm7_none_exact' / 'hessian.npy').exists()
+
+
+def test_parsl_path(xyz_path, file_dir):
+    with open(devnull, 'w') as fo:
+        with redirect_stdout(fo):
+            main([
+                str(xyz_path), '--exact', '--method', 'pm7', 'None',
+                '--parsl-config', str(file_dir / 'example_config.py')
+            ])
+    assert (Path('run') / 'water' / 'pm7_none_exact' / 'hessian.npy').exists()
