c63d51c5e319bbf581922027643bad49edcbd560
Logan Ward <ward.logan.t@gmail.com>
2023-12-08 10:23:42 -0500

    Add a progress tracker

--- jitterbug/thinkers/exact.py
+++ jitterbug/thinkers/exact.py
@@ -25,9 +25,11 @@ class ExactHessianThinker(BaseThinker):
         self.unperturbed_energy: Optional[float] = None
         self.single_perturb = np.zeros((len(atoms), 3, 2)) * np.nan  # Perturbation of a single direction. [atom_id, axis (xyz), dir_id (0 back, 1 forward)]
         self.double_perturb = np.zeros((len(atoms), 3, 2, len(atoms), 3, 2)) * np.nan
+        self.total_required = len(atoms) * 3 * 2 + (len(atoms) * 3) * (len(atoms) * 3 - 1) * 2 + 1
         # Perturbation of two directions [atom1_id, axis1, dir1_id, atom2_id, axis2 dir2_id]
 
         # Load what has been run already
+        self.total_complete = 0
         self.run_dir = run_dir
         self.run_dir.mkdir(exist_ok=True)
         self.energy_path = self.run_dir / 'unperturbed.energy'
@@ -36,6 +38,7 @@ class ExactHessianThinker(BaseThinker):
 
         if self.energy_path.exists():
             self.unperturbed_energy = float(self.energy_path.read_text())
+            self.total_complete += 1
 
         if self.single_path.exists():
             with self.single_path.open() as fp:
@@ -44,6 +47,7 @@ class ExactHessianThinker(BaseThinker):
                     index = tuple(map(int, row[:-1]))
                     count += 1
                     self.single_perturb[index] = row[-1]
+            self.total_complete += count
             self.logger.info(f'Read {count} single perturbations out of {self.single_perturb.size}')
 
         if self.double_path.exists():
@@ -56,7 +60,9 @@ class ExactHessianThinker(BaseThinker):
                     sym_index = list(index)
                     sym_index[:3], sym_index[3:] = index[3:], index[:3]
                     self.double_perturb[index] = self.double_perturb[tuple(sym_index)] = row[-1]
-            self.logger.info(f'Read {count} double perturbations')
+                to_do = (len(atoms) * 3) * (len(atoms) * 3 - 1) * 2
+            self.total_complete += count
+            self.logger.info(f'Read {count} double perturbations out of {to_do}')
 
     @agent()
     def submit_tasks(self):
@@ -137,6 +143,7 @@ class ExactHessianThinker(BaseThinker):
         calc_type = result.task_info['type']
         atoms = read_from_string(result.value, 'extxyz')
         energy = atoms.get_potential_energy()
+        self.total_complete += 1
 
         # Store unperturbed energy
         if calc_type == 'unperturbed':
@@ -147,7 +154,9 @@ class ExactHessianThinker(BaseThinker):
 
         # Store perturbed energy
         coord = result.task_info['coord']
-        self.logger.info(f'Saving a {calc_type} perturbation: ({",".join(map(str, coord))})')
+        self.logger.info(f'Saving a {calc_type} perturbation: ({",".join(map(str, coord))}).'
+                         f' Progress: {self.total_complete}/{self.total_required}'
+                         f' ({self.total_complete/self.total_required * 100:.2f}%)')
         if calc_type == 'single':
             energy_file = self.single_path
             energies = self.single_perturb
