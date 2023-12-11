5e178af9dc68fb1c56e2c888b9a59b9f41560151
Logan Ward <WardLT@users.noreply.github.com>
2023-12-11 10:24:27 -0500

    Add a simple sampling strategy (#34)
    No rotations or other nonesense. Just bump each atom

--- notebooks/1_explore-sampling-methods/run-all-methods.sh
+++ notebooks/1_explore-sampling-methods/run-all-methods.sh
@@ -3,23 +3,23 @@
 xyz=../data/exact/caffeine_pm7_None.xyz
 method='pm7/None'
 
-for step_size in 0.02; do
+for step_size in 0.005 0.01 0.02; do
     # Do the randomized methods
-    for notebook in 0_random-directions-same-distance.ipynb 1_random-directions-variable-distance.ipynb; do
+    for notebook in 0_random-directions-same-distance.ipynb 1_random-directions-variable-distance.ipynb 4_simple-unfirom.ipynb; do
         papermill -p starting_geometry $xyz -p method $method -p step_size $step_size $notebook last.ipynb
     done
     
     # Test with different reductions for "along axes"
     notebook=2_displace-along-axes.ipynb
-    for n in 2 4 8; do
+    for n in 4; do
         papermill -p starting_geometry $xyz -p method $method -p perturbs_per_evaluation $n -p step_size $step_size $notebook last.ipynb
     done
 done
 
 # Test with the vibrational modes
 notebook=3_displace-along-vibrational-modes.ipynb
-for step_size in 0.001 0.002; do  # These step sizes are energy scales in eV, not distances in Angstrom as above
-    for n in 16 32 64; do
+for step_size in 0.0025; do  # These step sizes are energy scales in eV, not distances in Angstrom as above
+    for n in 32; do
         papermill -p starting_geometry $xyz -p method $method -p perturbs_per_evaluation $n -p step_size $step_size $notebook last.ipynb
     done
 done
